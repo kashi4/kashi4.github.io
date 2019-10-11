cae2ff0029856190e71b90eba86f0d4e9ce42dd8
KashihakumwaJ <kas16003@byui.edu>
2019-10-11 10:04:17 -0600

    changes

---
+++ cit230/lesson4/scripts/main.js
@@ -0,0 +1,27 @@
+var currentDate = new Date();
+var currentDateString = new Date();
+
+// get day of the week
+var weekDayNumber = currentDate.getDay();
+
+var daysOfWeek = [
+    'Sunday','Monday','Tuesday','Wednesday',
+    'Thursday','Friday','Saturday',
+
+];
+
+var weekDay = daysOfWeek[weekDayNumber];
+currentDateString = weekDay + ', ';
+currentDateString += currentDate.getDate();
+var months = [
+    'January','February','March','April','May','June','July','August',
+    'September','October', 'November', 'December',
+];
+
+var monthNumber = currentDate.getMonth();
+var month = months[monthNumber];
+
+currentDateString += ' ' + month;
+currentDateString += ' ' + currentDate.getFullYear();
+document.getElementById('currentDate').innerHTML = currentDateString;
+//debugging
