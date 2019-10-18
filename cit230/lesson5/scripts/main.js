cb340f2d1a376da39bf8eb874d5f77899146e40e
KashihakumwaJ <kas16003@byui.edu>
2019-10-18 10:57:31 -0600

    changes

--- cit230/lesson5/scripts/main.js
+++ cit230/lesson5/scripts/main.js
@@ -28,6 +28,21 @@ document.getElementById('currentDate').innerHTML = currentDateString;
 
 // create 5 day forcats
 
+let currentDay = weekDayNumber;
+for (let i =1; i < 6; i++) {
+    // add one day to current day
+    currentDay++;
+}
 
+// if current day is greater than 6 (Saturday), reset it to zero
+
+if (currentDay > 6) {
+    currentDay = 0;
+}
+
+//assign value to placeholder in HTML
+
+const element = document.getElementById('day${i}');
+element.innerHTML = daysOfWeek[currentDay];
 
 
