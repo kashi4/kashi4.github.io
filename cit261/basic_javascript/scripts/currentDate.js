ef620b5810d91a0fbb86aeab22d8f0ed86912a8d
KashihakumwaJ <kas16003@byui.edu>
2019-10-28 06:17:30 -0600

    chnages

--- cit261/basic_javascript/scripts/currentDate.js
+++
@@ -1,7 +0,0 @@
-onload = function(){
-    document.getElementById("year").innerHTML = new Date().getFullYear();
-    document.getElementById("lastModified").innerHTML = "Page last changed " + document.lastModified.split(" ")[0];
-}
-
-//DEBUG:
-console.log(lastModified)
