2a5bea347f7b83f7516ac6dec74776b94cbdbf76
KashihakumwaJ <kas16003@byui.edu>
2019-10-28 07:42:07 -0600

    changes

---
+++ cit261/topic1/scripts/main.js
@@ -0,0 +1,48 @@
+var currentDate = new Date();
+var currentDateString = new Date();
+
+// get day of the week
+var weekDayNumber = currentDate.getDay();
+
+var daysOfWeek = [
+    'Sunday','Monday','Tuesday','Wednesday',
+    'Thursday','Friday','Saturday',
+
+];
+
+var weekDay = daysOfWeek[weekDayNumber];
+currentDateString = weekDay + ', ';
+currentDateString += currentDate.getDate();
+var months = [
+    'January','February','March','April','May','June','July','August',
+    'September','October', 'November', 'December',
+];
+
+var monthNumber = currentDate.getMonth();
+var month = months[monthNumber];
+
+currentDateString += ' ' + month;
+currentDateString += ' ' + currentDate.getFullYear();
+document.getElementById('currentDate').innerHTML = currentDateString;
+//debugging
+
+et currentDay = weekDayNumber;
+for (let i =1; i < 6; i++) {
+    // add one day to current day
+    currentDay++;
+
+
+// if current day is greater than 6 (Saturday), reset it to zero
+
+if (currentDay > 6) {
+    currentDay = 0;
+}
+const element = document.getElementById(`day${i}`);
+element.innerHTML = daysOfWeek[currentDay];
+}
+//assign value to placeholder in HTML
+
+if (weekDayNumber === 5){
+    document.getElementById("pancake").removeAttribute("class","hidden");
+} 
+
