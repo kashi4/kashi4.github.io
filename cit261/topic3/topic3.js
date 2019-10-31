e74ddb89c2ed8fbd66ecc786b126f26cc16e9743
KashihakumwaJ <kas16003@byui.edu>
2019-10-31 04:55:48 -0600

    changes

--- cit261/topic3/topic3.js
+++ cit261/topic3/topic3.js
@@ -1,5 +1,31 @@
-var x = myFunction(4, 3);
-document.getElementById("demo").innerHTML = x;
-function myFunction(a, b) {
-  return a * b;
-}
+// Objects
+
+/*let myFamily = {
+  'father':'charles', 'mother': 'lina','child1':'theocha',
+  'child2':'letisha','child3':'theo'
+};
+console.log(myFamily.father,myFamily.child3);
+
+// Arrays
+
+let myFamily = ['father','mother','child1','child2','child3'];
+  
+ console.log(myFamily[1]); */
+
+ // JSON IN ACTION
+
+ let myFamilies = [
+   {
+    'father':'charles', 'mother': 'lina','child1':'theocha',
+  'child2':'letisha','child3':'theo'
+   },
+   {
+    'father':'Tsediso', 'mother': 'Edna','child1':'Nala',
+  'child2':'  junior','child3':'unborn'
+   },
+   {
+    'father':'Justin', 'mother': 'Loini','child1':'Helam',
+  'child2':'Lora','child3':'stillComing'
+   }
+ ]
+console.log(myFamilies[2].child3);
