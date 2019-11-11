c1da4af5d7fcec289dbff07d3a0d020b72bf46a7
KashihakumwaJ <kas16003@byui.edu>
2019-11-10 18:31:30 -0700

    changes

--- cit261/topic1/scripts/topic1.js
+++ cit261/topic1/scripts/topic1.js
@@ -1,6 +1,26 @@
 
 //loops - for loop
 
-for(let i = 0; i <= 10; i++){
-    console.log(`count the number of times:${i}`);
-}
+function simpleLoop()
+               {
+                   var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+                   
+                   var string1 = [];
+                   
+                   for(i = 0; i < alphabet.length; i++)
+                       {
+                           string1.push(alphabet[i]);
+                           document.getElementById("loop").innerHTML += "The value of index #" + i + " in the array is: " + string1[i] + "<br>";
+                       }
+                   document.getElementById("loopBtn").style = "display:none;";
+                   document.getElementById("showLoop").style = "display:inline;";
+                   document.getElementById("hideLoop").style = "display:inline;";
+               }
+            function showLoop()
+               {
+                   document.getElementById("loop").style = "display:block;";
+               }
+            function hideLoop()
+               {
+                   document.getElementById("loop").style = "display:none;";
+               }
