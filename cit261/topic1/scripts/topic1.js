ac793838541499c41bf152929523510507d2ac93
KashihakumwaJ <kas16003@byui.edu>
2019-11-10 18:20:42 -0700

    changes

--- cit261/topic1/scripts/topic1.js
+++ cit261/topic1/scripts/topic1.js
@@ -1,12 +1,3 @@
-// The tempreture in kelvin for today
-const kelvin = 310;
-//converting kelvin to celsius
-const celsius = kelvin - 273;
-// The equation to calclute fahrenheit from celcius
-let fahrenheit = (celsius * (9/5) + 32);
-//using the .floor math to get rid of the decimal number
-fahrenheit = Math.floor(fahrenheit);
-console.log(`The tempreture is ${fahrenheit} degrees Fahrenheit.`);
 
 //loops - for loop
 
