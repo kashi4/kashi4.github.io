950b8aa0342444618dbe0ad6c1142c10f7631f6d
KashihakumwaJ <kas16003@byui.edu>
2019-11-23 22:56:37 -0700

    changes

--- cit230/lesson10/scripts/main.js
+++ cit230/lesson10/scripts/main.js
@@ -49,3 +49,46 @@ if (weekDayNumber === 5){
     document.getElementById("pancake").removeAttribute("class","hidden");
 } 
 
+// Fetch the data
+// function getWeather()
+
+const apiForecastURL = 'http://api.openweathermap.org/data/2.5/forecast/?id=5604473&appid=4382ef58145d424ca7ee8af38e82d6d6&units=imperial';
+fetch(apiForecastURL)
+.then(
+    (response) => response.json()
+
+)
+.then(
+    (forecast) => {
+        let nextDate = new Date();
+        nextDate.setDate(nextDate.getDate() + 1);
+        let dateString = getDateString(nextDate);
+        let hourString = '18:00:00';
+        let counter = 1;
+     
+        
+        // loop through results
+        forecast.list.forEach(
+            (forecast) => {
+                if(forecast.dt_txt.includes(dateString) &&
+                forecast.dt_txt.includes(hourString)) {
+                
+                    const element = document.getElementById(`temp${counter}`);
+                    element.innerHTML = forecast.main.temp + '&deg';
+
+                    // increment variables
+                    counter += 1;
+                    nextDate.setDate(nextDate.getDate() + 1);
+                    dateString = getDateString(nextDate);
+                }
+            } 
+        );
+    }
+);
+
+function getDateString(date) {
+    let dateString = date.getFullYear() + '-' +
+     (date.getMonth() + 1) + '-' + 
+     date.getDate();
+    return dateString;
+}
