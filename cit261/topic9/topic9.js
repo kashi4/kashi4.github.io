c11054ceb8a702430a8c97bcac0a0178c6629b87
KashihakumwaJ <kas16003@byui.edu>
2019-12-01 19:51:15 -0700

    changes

--- cit261/topic9/topic9.js
+++ cit261/topic9/topic9.js
@@ -1,16 +1,25 @@
-var content = document.getElementById("content");
-var button = document.getElementById("display more");
+/*var body = document.getElementById('body');
+body.addEventListener('touchstart',touchFunction,false);
 
-button.onclick = function() {
-    if (content.className == "open"){
-        // shrink the box
-        content.className = "";
-        button.innerHTML = "display more";
-    } else {
-        // expand the box
-        content.className = "open";
-        button.innerHTML = "display less";
-    }
+function touchFunction(addEventListener){
+    var touch = addEventListener.changeTouches;
+    var i = 0;
+    var x = event.changeTouches[i].pageX;
+    var x = event.changeTouches[i].pageY;
+    document.getElementById("outputDiv").innerHTML = "You see this because a touch triggered a touch event!<br>" + " X position: " + x + "<br> Y position:" + y;
+}*/
 
-};
 
+function clicky(){
+    document.getElementById("barca").classList.toggle("hide");
+    console.log("button");
+}
+
+function barca(){
+    document.getElementById("barcaDiv").classList.toggle("color");
+    console.log("barca");
+}
+
+function myFunction() {
+    alert("Page is loaded");
+  }
