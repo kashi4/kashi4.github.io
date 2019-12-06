b002475a917ec7b3094348a915ae5473a0d89a41
KashihakumwaJ <kas16003@byui.edu>
2019-12-06 12:03:16 -0700

    changes

---
+++ templeProject/scripts/temples.js
@@ -0,0 +1,14 @@
+[
+    {
+        "TempleName":"Accra Ghana Temple",
+        "TempleLocation":"Accra Ghana ",
+        "Services":[
+            "No clothing rental available",
+            "No cafeteria available",
+            "Patron housing available",
+            "Distribution center nearby",        
+        
+        ]
+        
+    },
+]
